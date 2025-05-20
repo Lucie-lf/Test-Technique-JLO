7aab6ef23d928ded95697116ac76730ee8e18cd4
Lucie-lf <lucie.le-foll@epitech.eu>
2025-05-20 16:25:23 +0200

    Debut du front

---
+++ src/pages/Done.tsx
@@ -0,0 +1,8 @@
+export default function Done() {
+    return (
+      <div>
+        <h1>Done</h1>
+        <p>List of finished tasks</p>
+      </div>
+    )
+  }
---
+++ src/pages/ToDo.tsx
@@ -0,0 +1,8 @@
+export default function ToDo() {
+  return (
+    <div>
+      <h1>To Do</h1>
+      <p>List of tasks to be done</p>
+    </div>
+  )
+}
